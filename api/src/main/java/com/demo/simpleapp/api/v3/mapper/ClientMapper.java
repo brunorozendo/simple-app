23f7626c39ded268dbb40ffeef43d7df3473ed72
Bruno Rozendo <bruno@brunorozendo.com>
2022-07-18 19:14:42 -0300

    add custon plugin to gradle

--- api/src/main/java/com/demo/simpleapp/api/v3/mapper/ClientMapper.java
+++ api/src/main/java/com/demo/simpleapp/api/v3/mapper/ClientMapper.java
@@ -4,10 +4,13 @@ package com.demo.simpleapp.api.v3.mapper;
 import com.demo.simpleapp.api.v3.controller.dto.ClientResponse;
 import com.demo.simpleapp.api.v3.dao.models.Client;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ClientMapper {
 
+
+    @Mapping(target = "customerInfo", source = ".")
     ClientResponse convert(Client source);
 
 
